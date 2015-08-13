7954b7b306f5ed5576fdf0a9fc766c5c6e4bdbf4
Janus <ysangkok@gmail.com>
2015-08-13 23:44:24 +0200

    just useful

--- baconbot.py
+++ baconbot.py
@@ -5,161 +5,33 @@ import logging
 import asyncio
 import irc3
 from irc3.plugins.command import command
-import pyinotify
 from eval_arith import comp_expr, EvalConstant
-from irc3.plugins.cron import cron
-import os
-import subprocess
-import traceback
-import sys
 
 logging.basicConfig(filename='bot.log',level=logging.DEBUG)
 logger = logging.getLogger("BACONBOT")
 
-inotifymask = pyinotify.IN_DELETE | pyinotify.IN_CREATE  # watched events TODO FIXME should be EDITED
-
 from regner_det import get_weather
-import time
 
-#wdd = wm.add_watch('/tmp', inotifymask, rec=True)
-
-class Command(object):
-    def __init__(self, name, target, func, loop, callback):
-        logger.debug("command")
-        wm = pyinotify.WatchManager()
-        self.notifier = pyinotify.AsyncioNotifier(wm, loop, callback=callback)
-        self.name = "".join(x for x in name if x.isalnum())
-        self.target = target
-        self.func = func
-        try:
-            os.mkdir("/tmp/outputs/{}".format(name))
-        except FileExistsError:
-            pass
-        wm.add_watch("/tmp/outputs/{}/".format(name), inotifymask, rec=True)
-    def ONDELETE(self): # TODO FIXME XXX CALL THIS
-        wm.rm_watch(arg[0], bool(int(arg[2])), quiet=True)
-        assert name
-        os.system("rm -rf /tmp/outputs/{}".format(name))
-    def __str__(self):
-        return "Command({}, {}, {})".format(self.name, self.target, self.func)
+jobs = [ ("raining", "#shelloutput", get_weather) ]
 
 @irc3.plugin
 class SwitchControllerPlugin(object):
-    @command
-    def debug(self, mask, target, args):
-        """ Debug
-
-        %%debug
-        """
-        pdb.set_trace()
-
-    @irc3.event(irc3.rfc.JOIN)
-    @asyncio.coroutine
-    def get_inotify(self, mask, channel):
-        def fun():
-          yield from asyncio.create_subprocess_shell(
-            "python3 inotify.py /tmp/outputs",
-            loop=self.bot.loop
-          )
-        yield from ("prefix{}".format(i) for i in fun())
-
     @command
     def calc(self, mask, target, args):
         """ Calc command
 
         %%calc <expr>
         """
-        EvalConstant.vars_ = {'A': 0, 'B': 1.1, 'C': 2.2, 'D': 3.3, 'E': 4.4, 'F': 5.5, 'G': 6.6, 'H':7.7, 'I':8.8, 'J':9.9}
         res = comp_expr.parseString(args["<expr>"])[0].eval()
         self.bot.notice(target, str(res))
 
     @command
-    def add_command(self, mask, target, args):
-        """ Add cron command
-
-        %%add_command <name> <shell> <shell>...
-        """
-
-        def callback(s):
-            logger.debug(s)
-            self.bot.notice(target, repr(s.proc_fun()))
-            self.bot.notice(target, str(s.proc_fun()))
-        self.jobs += [Command(args["<name>"], target, " ".join(args["<shell>"]), self.bot.loop, callback)]
-
-    @command
-    def rm_command(self, mask, target, args):
-        """ Remove cron command
-
-        %%rm_command <name>
-        """
-        self.jobs = list(filter(lambda x: x.name != args["<name>"], self.jobs))
-
-    @command
-    def list_commands(self, mask, target, args):
-        """ List commands
-
-        %%list_commands
-        """
-        for job in self.jobs:
-             self.bot.notice(target, str(job))
-
-    #@cron('0 */2 * * *')
-    @cron('0 */12 * * *')
-    @asyncio.coroutine
-    def check_periodic(self):
-        yield from self.run_jobs()
-
-    @command
     def run_jobs(self, mask, target, args):
-        """ Run jobs
-
-        %%run_jobs
-        """
-        yield from self.do_run_jobs()
-
-    @asyncio.coroutine
-    def do_run_jobs(self):
-        for j in self.jobs:
-            i = j.func
-            self.bot.notice(j.target, "now processing {}".format(j.name))
-            stdout = None
-            p = None
-            try:
-                if type(i) == str:
-                    with open("/tmp/outputs/{}/output".format(j.name), "w") as f:
-                        p = subprocess.Popen(i, shell=True, stdout=f, cwd="/tmp/outputs/{}".format(j.name))
-                        p.wait(timeout=5)
-                    user_stuff = "command {}, exit status {}".format(i, p.returncode)
-                    stdout = p.stdout
-                else:
-                    user_stuff = i()
-                    if user_stuff is None: user_stuff = "no rain"
-                    assert type(user_stuff) == str, type(user_stuff)
-            except Exception as e:
-                print(e.__class__)
-                traceback.print_exc()
-                if p: p.terminate()
-                stdout = str(e)
-                user_stuff = str(e)
-            self.bot.notice(j.target, "job {}: {}".format(j.name, user_stuff))
-
-    @command
-    def alarm(self, mask, target, args):
-        """Alarm command
-        %%alarm [ARG]
-        """
-        arg = args.get('ARG', None)
-        if arg:
-            logging.info('Starting alarm for {} seconds'.format(arg))
-            # self.bot.notice(mask.nick, 'ALARM ALARM ALARM I {} SEKUNDER!'.format(arg))
-        self.bot.loop.create_task(
-            self.process_maybe_timed_command(
-                target, 1, arg
-            )
-        )
+        for job in jobs:
+            self.bot.notice(target, job)
+            self.bot.notice(target, job[2]())
 
     def __init__(self, bot):
-        self.jobs = [ Command("raining", "#shelloutput", get_weather, asyncio.get_event_loop(), lambda s: bot.notice("#shelloutput", s.proc_fun())) ]
         self.bot = bot
         self.switches = self.bot.db.get('switches', None)
         if not self.switches:
